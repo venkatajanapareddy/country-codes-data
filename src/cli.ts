d27b5c1be275b2ddc1fe867505889b5d14bd3575
Venkata Janapareddy <venkatajanapareddy9@gmail.com>
2025-05-06 00:51:57 -0500

    Update to v1.0.1 with consistent binary names

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,7 @@ import { getCountryByCode } from './index';
 
 const code = process.argv[2];
 if (!code) {
-  console.error('Usage: npx country-codes [CODE] or npx countrycodes [CODE]');
+  console.error('Usage: npx country-codes-data [CODE] or npx ccd [CODE]');
   process.exit(1);
 }
 const country = getCountryByCode(code);
